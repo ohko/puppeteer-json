2531aef7182c5ebd888a03abfe6d0fe3b12a8823
Microanswer <microanswer@outlook.com>
2020-10-19 00:36:15 +0800

    优化文档。

--- runtime/sample.ts
+++ runtime/sample.ts
@@ -1,92 +1,53 @@
 import * as base from "./base"
 
 export const Sample: base.IData = {
-   Comment: "演示在baidu查找苹果APPL股票价格",
+   Comment: "在天猫获取指定的商品评论",
    Json: [
-      {
-         Cmd: base.CmdTypes.Sub, Comment: "定义一组操作集合", Value: "sub1", Json: [
-            { Cmd: base.CmdTypes.ClosePage, Comment: "关闭页面" },
-            { Cmd: base.CmdTypes.Shutdown, Comment: "关闭程序" },
+
+      {Cmd: base.CmdTypes.Finally, Comment: "最终结束操作",
+         Json: [
+            {Cmd: base.CmdTypes.Var, Comment: "清理脏字段", Key: "tabCount", SyncEval: ""},
+            {Cmd: base.CmdTypes.Shutdown, Comment: "关闭浏览器"}
          ]
       },
-      { Cmd: base.CmdTypes.BootPuppeteer, Comment: "启动Puppeteer", Options: { headless: false, args: ["--no-sandbox"], defaultViewport: null, ignoreDefaultArgs: ["--enable-automation"] } },
-      {
-         Cmd: base.CmdTypes.Finally, Comment: "无论如何，最终执行一些清理操作", Json: [
-            { Cmd: base.CmdTypes.Call, Comment: "调用操作集合", Value: "sub1" }
+
+      {Cmd: base.CmdTypes.BootPuppeteer, Comment: "打开Puppeteer自带浏览器", Options: { defaultViewport: null, headless: false}},
+      {Cmd: base.CmdTypes.Wait, Comment: "等待2秒", Value: "2000"},
+      {Cmd: base.CmdTypes.NewPage, Comment: "打开新tab页"},
+      {Cmd: base.CmdTypes.ShowMouse, Comment: "显示鼠标"},
+      {Cmd: base.CmdTypes.Wait, Comment: "等待2秒", Value: "2000"},
+      {Cmd: base.CmdTypes.Navation, Comment: "打开天猫首页", Key: "url", Options: {waitUntil: "networkidle2"}},
+
+      {Cmd: base.CmdTypes.Type, Comment: "输入关键词", Key: "keyword", Selector: "#mq"},
+      {Cmd: base.CmdTypes.Click, Comment: "点击搜索按钮", Selector: "[type='submit']", WaitNav: true},
+      {Cmd: base.CmdTypes.Wait, Comment: "等待5秒", Value: "5000"},
+
+      {Cmd: base.CmdTypes.Click, Comment: "点击搜索结果第八个", Selector: ".product[data-id] .product-iWrap .productImg-wrap", Index: "7"},
+      {Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000"},
+
+      {Cmd: base.CmdTypes.PagesCount, Comment: "获取当前所有tab数量", Key: "tabCount"},
+      {Cmd: base.CmdTypes.ActivePage, Comment: "切换到最后一个tab", Key: "tabCount"},
+
+      {Cmd: base.CmdTypes.ShowMouse, Comment: "切换新tab后重新设置显示鼠标"},
+      {Cmd: base.CmdTypes.ReloadPage, Comment: "重新加载页面保证鼠标显示正确", Options: {waitUntil: "networkidle2"}},
+
+      {Cmd: base.CmdTypes.Wait, Comment: "等待2秒", Value: "2000"},
+      {Cmd: base.CmdTypes.ExistsSelector, Comment: "存在登录提示的话就关闭登录提示", Selector: "#sufei-dialog-close",
+         Json: [
+            {Cmd: base.CmdTypes.Click, Comment: "关闭登录提示", Selector: "#sufei-dialog-close"},
+            {Cmd: base.CmdTypes.Wait, Comment: "等待1秒", Value: "1000"}
          ]
       },
-      { Cmd: base.CmdTypes.HttpGet, Comment: "获取IP", Key: "ip1", Value: "http://ip.lyl.hk" },
-      { Cmd: base.CmdTypes.Js, Comment: "高级JS指令", AsyncEval: "return axios.default.get('http://ip.lyl.hk').then(x=>{return {ip2:x.data}}).catch(x=>{return {ip2:x.toString()}})" },
-      { Cmd: base.CmdTypes.Var, Comment: "设置循环次数", Key: "count", SyncEval: "5" },
-      {
-         Cmd: base.CmdTypes.Loop, Comment: "循环count次", Key: "count", Json: [
-            { Cmd: base.CmdTypes.Break, Comment: "跳出循环", SyncEval: "loopCounter>2" },
-            { Cmd: base.CmdTypes.Log, Comment: "记录日志", SyncEval: "'循环'+array[loopCounter]+'次'" },
-         ]
-      },
-      { Cmd: base.CmdTypes.Var, Comment: "设置循环次数", Key: "count", SyncEval: "5" },
-      {
-         Cmd: base.CmdTypes.Loop, Comment: "循环count次", Key: "count", Json: [
-            {
-               Cmd: base.CmdTypes.Condition, Comment: "判断loopCounter",
-               Conditions: [
-                  {
-                     Condition: "loopCounter==0", Json: [
-                        { Cmd: base.CmdTypes.Log, Comment: "记录日志", SyncEval: "'循环到第1次了'" },
-                     ]
-                  },
-                  {
-                     Condition: "loopCounter>1", Json: [
-                        { Cmd: base.CmdTypes.Break, Comment: "跳出Loop循环", SyncEval: "true" },
-                     ]
-                  },
-                  {
-                     Condition: "loopCounter==1", Json: [
-                        { Cmd: base.CmdTypes.Log, Comment: "记录日志", SyncEval: "'循环到第2次了'" },
-                     ]
-                  }
-               ]
-            }
-         ]
-      },
-      {
-         Cmd: base.CmdTypes.If, Comment: "记录1", SyncEval: "'1'==1", Json: [
-            { Cmd: base.CmdTypes.Var, Comment: "记录1", Key: "记录1", SyncEval: "'记录1'" },
-         ]
-      },
-      {
-         Cmd: base.CmdTypes.If, Comment: "不会记录2", SyncEval: "'1'===1", Json: [
-            { Cmd: base.CmdTypes.Var, Comment: "不会记录2", Key: "记录2", SyncEval: "'不会记录2'" },
-         ]
-      },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.NewPage, Comment: "创建新页面" },
-      { Cmd: base.CmdTypes.ShowMouse, Comment: "显示鼠标" },
-      { Cmd: base.CmdTypes.SetHeader, Comment: "设置Header", Options: { "Accept-Language": "zh-CN,zh;q=0.9" } },
-      { Cmd: base.CmdTypes.SetTimeout, Comment: "设置默认打开页面超时时间", Key: "timeout" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.NewPage, Comment: "创建新页面" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.Var, Comment: "激活第二页", Key: "index", SyncEval: "2" },
-      { Cmd: base.CmdTypes.ActivePage, Comment: "激活第二页", Key: "index" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.ClosePage, Comment: "关闭第二页" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.ClosePage, Comment: "关闭当前页面" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.Navation, Comment: "打开百度", Key: "url" },
-      { Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000" },
-      { Cmd: base.CmdTypes.Type, Comment: "输入内容", Selector: "#kw", Key: "keyword", ScreenshotBefore: true, ScreenshotBehind: true },
-      { Cmd: base.CmdTypes.Click, Comment: "点击搜索", Selector: "#su", ScreenshotBefore: true, ScreenshotBehind: true, ScreenshotFull: true },
-      { Cmd: base.CmdTypes.TextContent, Comment: "获取textContent", Selector: ".op-stockdynamic-moretab-cur-num", Key: "price" },
-      { Cmd: base.CmdTypes.ClosePage, Comment: "关闭当前页面" },
+
+      {Cmd: base.CmdTypes.Click, Comment: "点击评论取按钮", Selector: "#J_TabBar [href='#J_Reviews']"},
+      {Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000"},
+
+      {Cmd: base.CmdTypes.TextContent, Comment: "获取第一条评论的内容", Key: "talkTxt", Selector: "#J_Reviews .rate-grid tr td.tm-col-master", Index: "0"},
+
+      {Cmd: base.CmdTypes.Wait, Comment: "等待4秒", Value: "4000"}
    ],
    DB: {
-      url: "https://www.baidu.com",
-      ipurl: "http://ip.lyl.hk",
-      keyword: "AAPL",
-      count: "0",
-      timeout: "5000",
-      array: ["一", "二", "三", "四"],
+      url: "http://www.tmall.com/",
+      keyword: "type c",
    }
 }
--- test.ts
+++ test.ts
@@ -5,7 +5,7 @@ import * as sample from "./runtime/sample";
 import * as base from "./runtime/base";
 
 (async _ => {
-
+   process.env.DEBUG = "1";
    let no: Number = 0, data: any = "SUCCESS", result: base.IResult;
    const run = new runtime.Runtime()
    try {
@@ -14,8 +14,16 @@ import * as base from "./runtime/base";
       no = 1, data = e.message
    } finally {
       result = run.SyncGetResult()
-      for (let i in result.Screenshots) result.Screenshots[i] = result.Screenshots[i].substr(0, 50)
-      console.log("RESULT:", { No: no, Data: (typeof data == "string" ? data : JSON.stringify([data])), DB: result.DB, Logs: result.Logs, Screenshot: result.Screenshots, Origin: JSON.stringify(sample.Sample) })
+      for (let i in result.Screenshots) {
+         result.Screenshots[i] = result.Screenshots[i].substr(0, 50);
+      }
+      console.log("RESULT:", {
+         No: no,
+         Data: (typeof data == "string" ? data : JSON.stringify([data])),
+         DB: result.DB,
+         // Logs: result.Logs,
+         // Screenshot: result.Screenshots, Origin: JSON.stringify(sample.Sample)
+      });
    }
 
    process.exit()
